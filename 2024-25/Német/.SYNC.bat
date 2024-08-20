0bbb65d78cc56bea05d573f9d32dda2a989d10ee
NAdaM02 <adam.ancsin1@gmail.com>
2024-08-20 12:19:30 +0200

    08. 20. - 12:19

---
+++ 2024-25/Német/.SYNC.bat
@@ -0,0 +1,26 @@
+@echo off
+
+:: Add all files to the staging area
+git add --all
+
+for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
+    set "COMMIT_MSG=%%a:%%b"
+)
+set "COMMIT_MSG=%DATE% - %COMMIT_MSG%"
+
+git commit -m "%COMMIT_MSG%"
+
+git commit --amend -m "%COMMIT_MSG%"
+
+echo Pulling changes from the remote repository...
+git pull --tags origin main
+
+echo Pushing changes...
+git push --force --quiet
+
+echo.
+echo Upload successful.
+
+TIMEOUT /T 1 > nul
+
+exit
