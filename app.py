6cb14db7a4cdd4ba2c15a2e725636d4c4b71633f
NAdaM02 <adam.ancsin1@gmail.com>
2025-02-12 16:18:44 +0100

    02. 12. - 16:18

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 
 EXCLUDED_SUBJECTS = {"Németh", "Zeneirodalom", "Matek", "Fizika"}
 
-BASE_DIR = Path(__file__).resolve() / "2024-25"
-TEMP_DIR = Path(__file__).resolve() / "data"
+BASE_DIR = Path(__file__).resolve().parent / "2024-25"
+TEMP_DIR = Path(__file__).resolve().parent / "data"
 
 print("\n\n\nCurrent working directory:", Path.cwd())
 print("Expected BASE_DIR:", BASE_DIR)
