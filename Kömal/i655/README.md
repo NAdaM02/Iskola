674d9c4a0a41a9f5b0eece427e7ef062843e3d47
NAdaM <adam.ancsin1@gmail.com>
2025-04-01 15:16:41 +0200

    04. 01. - 15:16

---
+++ Kömal/i655/README.md
@@ -0,0 +1,2 @@
+A program a 3. verziójú python interpreterrel futtatható.
+'python main.py' vagy 'python3 main.py'
--- Kömal/i655/main.py
+++ Kömal/i655/main.py
@@ -1,3 +1,16 @@
+"""
+I. 655-ös feladat
+Ancsin Ádám, 11.C
+Budapesti Fazekas Mihály Gimnázium
+
+* megjegyzés:
+    A feladat példájában megadott kimenet első sorában a
+    kezdeti 1-es és 4-es számok a műveletsorozat elvégzése
+    utáni helyeit azonos (4-es) szám jelöli.
+    A helyes kimenet:
+     > 3 14 15 4
+     > 23 24 1 4
+"""
 
 def shuffle(arr:list, part_start:int, part_end:int, target_index:int):
     part_start, part_end, target_index = part_start-1, part_end-1, target_index
@@ -5,23 +18,30 @@ def shuffle(arr:list, part_start:int, part_end:int, target_index:int):
     rest = arr.copy()
     del rest[part_start:part_end+1]
     new = [0 for _ in range(len(arr))]
-    print(arr)
-    print(rest, part)
-    print()
-    for i in range(len(arr)):
-        if target_index-part_end+part_start-1<= i<= target_index-1:
-            new[i] = part.pop(0)
-            print('N', end=" ")
-        else:
-            new[i] = rest.pop(0)
-            print('.', end=" ")
-        print(i, new)
-    print()
+    if part_start<= target_index:
+        for i in range(len(arr)):
+            if target_index-part_end+part_start-1<= i<= target_index-1:
+                new[i] = part.pop(0)
+            else:
+                new[i] = rest.pop(0)
+    else:
+        for i in range(len(arr)):
+            if target_index<= i< target_index+1+part_end-part_start:
+                new[i] = part.pop(0)
+            else:
+                new[i] = rest.pop(0)
     return new
 
-a = shuffle([1, 2, 3, 4, 5, 6], 2, 3, 5)
-print(a)
-print()
-print('nigga')
-print()
-print(shuffle(a, 3, 5, 1))
+
+
+if __name__ == '__main__':
+    N, rep_count = map(int, input().split())
+    numbers = [i for i in range(1, N)]
+    
+    a = numbers.copy()
+    for _ in range(rep_count):
+        part_start, part_end, target_index = map(int, input().split())
+        a = shuffle(a, part_start, part_end, target_index)
+    
+    print(" ".join([str(a.index(interesting_number)+1) for interesting_number in numbers[:rep_count]]))
+    print(" ".join(list(map(str, a[:rep_count]))))
